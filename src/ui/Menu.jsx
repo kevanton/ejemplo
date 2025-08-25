341ed64966af23b5d3476933c3f5e473d9e590b9
kevanton <kevindelic@gmail.com>
2025-08-25 09:48:36 -0600

    cambio de colores en titulos

--- src/ui/Menu.jsx
+++ src/ui/Menu.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import '../App.css'
 
-const Menu = ({nombre, color}) => {
-    const apellidos = "alvarado angulo"
-    let clase_color
-    switch(color) {
-        case "rojo":
-            clase_color = "titulorojo"
-            break
-        case "verde":
-            clase_color = "tituloverde"
-            break
-    }
+const Menu = ({ nombre, color }) => {
+  const apellidos = 'alvarado anguloaaaa'
+  let clase_color
+  switch (color) {
+    case 'rojo':
+      clase_color = 'titulorojo'
+      break
+    case 'verde':
+      clase_color = 'tituloverde'
+      break
+  }
   return (
     <div>
-        <p className = {clase_color}>
-            {nombre} {apellidos}
-        </p>
+      <p className={clase_color}>
+        {nombre} {apellidos}
+      </p>
+      <p>{nombre}</p>
     </div>
   )
 }
