4e399369eb836df99c3a753ac48f1923e240ab85
Seraph <seraph.axl@gmail.com>
2025-08-25 09:14:05 -0600

    Creacion de component footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,41 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import reactLogo from './assets/react.svg'
+import Footer from './ui/Footer'
 import Menu from './ui/Menu'
+import viteLogo from '/vite.svg'
 
 function App() {
   const [count, setCount] = useState(0)
-const [nombre, setNombre] = useState("Ninguno")
-const [variable, setVariable] = useState ("Abel")
-useEffect (()=>{
-  if ( count == 5 ) {
-    setVariable("Abel Alvarado")
-    setNombre("kevin")
-  }
-}, [count])
+  const [nombre, setNombre] = useState('Ninguno')
+  const [variable, setVariable] = useState('Abel')
+  useEffect(() => {
+    if (count == 5) {
+      setVariable('Abel Alvarado')
+      setNombre('kevin')
+    }
+  }, [count])
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href='https://vite.dev' target='_blank'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href='https://react.dev' target='_blank'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          {variable}
-        </p>
+      <div className='card'>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <p>{variable}</p>
       </div>
-      <p className="read-the-docs">
-        {nombre}
-      </p>
-      <Menu nombre={nombre} color = "rojo"/>
-      <Menu nombre={variable} color ="verde"/>
-      <Menu nombre="variable" color ="rojo"/>
-      
-      
-        
+      <p className='read-the-docs'>{nombre}</p>
+      <Menu nombre={nombre} color='rojo' />
+      <Menu nombre={variable} color='verde' />
+      <Menu nombre='variable' color='rojo' />
 
+      <Footer />
     </>
   )
 }
---
+++ src/ui/Footer.jsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <div style={{ backgroundColor: 'black', padding: '10px', textAlign: 'center' }}>Este es el footer nuevo</div>
+}
+
+export default Footer
